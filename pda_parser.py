eeb7cb355234f3ee5a53576f3163a3ea2a07ae56
rafi-sama <rafimaliki28@gmail.com>
2023-11-25 12:47:43 +0700

    transtition rule dict

--- pda_parser.py
+++ pda_parser.py
@@ -1,15 +1,3 @@
-class transition:
-    def __init__(self, start_state, read, top_stack, final_state, pop, push):
-        self.start_state  = start_state
-        self.read = read
-        self.top_stack = top_stack
-        self.final_state = final_state
-        self.pop = pop
-        self.push = push
-
-    def __str__(self):
-        return f"{self.start_state} {self.read} {self.top_stack} {self.final_state} {self.pop} {self.push}"
-
 class pda:
     def __init__(self, states, symbols, stacksymbol, start_state, transition_rules):
         self.states = states
@@ -18,9 +6,7 @@ class pda:
         self.start_state = start_state
         self.transition_rules = transition_rules
 
-
 def read_pda(filepath):
-
     with open(filepath, 'r') as file:
         content = file.readlines()
 
@@ -32,10 +18,12 @@ def read_pda(filepath):
 
         for i in range(4, len(content)):
             transition_rule = content[i].split()
-            transition_rule = transition(transition_rule[0], transition_rule[1], transition_rule[2], transition_rule[3], transition_rule[4], transition_rule[5])
+            transition_rule = {(transition_rule[0], transition_rule[1], transition_rule[2]): (transition_rule[3], transition_rule[4], transition_rule[5])}
             transition_rules.append(transition_rule)
 
         return pda(states, symbols, stacksymbol, startstate, transition_rules)
+
+
 
 filepath = "pda/pda.txt"
 pda = read_pda(filepath)
