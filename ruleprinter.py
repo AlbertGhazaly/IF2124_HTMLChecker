badb1f189c569708a876aa7a3df53b3a3e1c800f
rafi-sama <rafimaliki28@gmail.com>
2023-11-26 16:06:22 +0700

    list rule

--- ruleprinter.py
+++ ruleprinter.py
@@ -11,10 +11,18 @@ stack = STACK(pda)
 states = set()
 counter = 1
 
+f = open("listRule.txt", "w")
+
 keys = pda.transition_rules.keys()
 for key in keys:
     print(f"{counter}. {key} {pda.transition_rules[key]}")
+    val = f"{counter}. {key} {pda.transition_rules[key]}\n"
+    states.add(key[0])
     counter += 1
+    print(counter)
+    f.write(f"{key} -> {pda.transition_rules[key]}\n")
+f.close()
+
 
 # print(len(keys))
 # print(states)
